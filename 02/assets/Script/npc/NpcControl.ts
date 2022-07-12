4e63a682959e2dbd55aae1175ad7146af23c5c9b
li_heng <1033797938@qq.com>
2022-07-12 18:02:20 +0800

    修正角色选择逻辑

--- 02/assets/Script/npc/NpcControl.ts
+++ 02/assets/Script/npc/NpcControl.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, Sprite, color, Color } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('NpcControl')
@@ -6,25 +6,33 @@ export class NpcControl extends Component {
     start() {
         // 女角色
         this.node.children[0].on(Node.EventType.MOUSE_DOWN, (Event)=> {
-            if(this.node.children[2].active == false && this.node.children[3].active == false) {
-                this.node.children[2].active = true;
-                this.node.children[3].active = false
-            }
-            if(this.node.children[2].active == false && this.node.children[3].active == true) {
-                this.node.children[2].active = true;
-                this.node.children[3].active = false;
-            }
+            this.node.children[0].getComponent(Sprite).color = new Color().fromHEX("#FAF9D7");
+            this.node.children[1].getComponent(Sprite).color = new Color().fromHEX("#FFFFFF");
+            this.node.children[2].active = true;
+            this.node.children[3].active = false;
+            // if(this.node.children[2].active == false && this.node.children[3].active == false) {
+            //     this.node.children[2].active = true;
+            //     this.node.children[3].active = false
+            // }
+            // if(this.node.children[2].active == false && this.node.children[3].active == true) {
+            //     this.node.children[2].active = true;
+            //     this.node.children[3].active = false;
+            // }
         })
         // 男角色
         this.node.children[1].on(Node.EventType.MOUSE_DOWN, (Event)=> {
-            if(this.node.children[2].active == false && this.node.children[3].active == false) {
-                this.node.children[3].active = true;
-                this.node.children[2].active = false;
-            }
-            if(this.node.children[3].active == false && this.node.children[2].active == true) {
-                this.node.children[3].active = true;
-                this.node.children[2].active = false;
-            }
+            this.node.children[0].getComponent(Sprite).color = new Color().fromHEX("#FFFFFF");
+            this.node.children[1].getComponent(Sprite).color = new Color().fromHEX("#FAF9D7");
+            this.node.children[2].active = false;
+            this.node.children[3].active = true;
+            // if(this.node.children[2].active == false && this.node.children[3].active == false) {
+            //     this.node.children[3].active = true;
+            //     this.node.children[2].active = false;
+            // }
+            // if(this.node.children[3].active == false && this.node.children[2].active == true) {
+            //     this.node.children[3].active = true;
+            //     this.node.children[2].active = false;
+            // }
         })
     }
 
